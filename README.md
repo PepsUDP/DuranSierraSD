6cb702e05a8c34155ff44b3f4168260cd44c15bb
Waskertyioup <abel.sierra_l@mail.udp.cl>
2022-05-02 00:45:01 -0400

    api

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # DuranSierraSD
+
+Correr API:
+
+(en la carpeta /API):
+docker build -t api .
+docker run -d -p 5000:5000 api
+
--- tarea1/API/app_service.py
+++ tarea1/API/app_service.py
@@ -1,8 +1,10 @@
 # Estructura basada en el tutorial:
 # https://medium.com/bb-tutorials-and-thoughts/how-to-dockerize-the-python-rest-api-with-flask-library-d2b51dd4a0ae
 # https://github.com/bbachi/python-flask-restapi
+# https://pynative.com/python-postgresql-select-data-from-table/
 
 import json
+import psycopg2
 
 class AppService:
 
@@ -14,5 +16,39 @@ class AppService:
         self.paramsJSON = json.dumps(self.params)
 
     def get_item(self, request_item):
+        connection = 0
+        #return json.dumps({'Try': "wopsie"});
+        try:
+            #return json.dumps({'Try': "dentro"});
+            connection = psycopg2.connect(user="postgres",
+                                    password="marihuana",
+                                    host="127.0.0.1",
+                                    port="5432",
+                                    database="tiendita")
+            cursor = connection.cursor()
+            return json.dumps({'Try': "conn"});
+            postgreSQL_select_Query = "select * from Items"
+
+            cursor.execute(postgreSQL_select_Query)
+            mobile_records = cursor.fetchall()
+
+            return json.dumps({'Try': mobile_records});
+
+            print("Print each row and it's columns values")
+            for row in mobile_records:
+                print("Name = ", row[1], )
+                print("Price = ", row[2])
+                print("Category  = ", row[3], "\n")
+
+        except (Exception, psycopg2.Error) as error:
+            print("Error while fetching data from PostgreSQL", error)
+
+        finally:
+            # closing database connection.
+            if connection:
+                cursor.close()
+                connection.close()
+                print("PostgreSQL connection is closed")
+
         return json.dumps({'query_item': request_item});
   
