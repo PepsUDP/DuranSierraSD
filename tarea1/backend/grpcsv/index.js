a2eca0e21b7ba077ed8aa3faf0f3599a86f39251
PepsUDP <70279893+PepsUDP@users.noreply.github.com>
2022-05-03 23:31:19 -0400

    Update index.js

--- tarea1/backend/grpcsv/index.js
+++ tarea1/backend/grpcsv/index.js
@@ -12,48 +12,7 @@ const protoLoader = require("@grpc/proto-loader");
 const PROTO_PATH = "./example.proto";
 const items = require("./data.json");
 
-
-
-
-
 const port = 5000;
-
-/*
-
-const options = {
-  keepCase: true,
-  longs: String,
-  enums: String,
-  defaults: true,
-  oneofs: true,
-};
-
-const packageDefinition = protoLoader.loadSync(PROTO_PATH, options);
-const itemProto = grpc.loadPackageDefinition(packageDefinition);
-
-const server = () => {
-  const server = new grpc.Server();
-  server.addService(itemProto.ItemService.service, {
-    getItem: (_, callback) => {
-      console.log("server query: ", _);
-      const itemName = _.request.name;
-      const item = items.item_list.filter((obj) => obj.name.includes(itemName));
-      callback(null, { items: item});
-    }
-  });
-  server.bindAsync("0.0.0.0:50051", grpc.ServerCredentials.createInsecure(), (err, port) => {
-    if (err != null) console.log(err);
-    else {
-      console.log("GRPC SERVER RUN AT http://localhost:50051");
-      server.start();
-    }
-  });
-};
-
-server();
-
-*/
-
 
 const server = require("./grpc_server");
 server.server();
